77aace45898d516d040d6f3518d147f2adf6d26f
Sponkrashov <adm07@CB00713.FESMSK.LOCAL>
2022-07-08 19:23:54 +0300

    херня

--- src/main/java/darvin/file_manager/InputOfUser.java
+++ src/main/java/darvin/file_manager/InputOfUser.java
@@ -3,6 +3,7 @@ package darvin.file_manager;
 import java.util.Scanner;
 
 public class InputOfUser {
+    FileManager fileManager = new FileManager("C:\\Users\\adm07\\Documents\\TestFolder");
 
     public void input() {
         Scanner scanner = new Scanner(System.in);
@@ -10,8 +11,28 @@ public class InputOfUser {
         String input = "";
 
         while (!input.equals(Commands.EXIT)) {
+            System.out.print(fileManager.getCurrentFolder() + ": ");
             input = scanner.nextLine();
-            System.out.println(input);
+            String tokens[] = input.split(" ");
+            String command = tokens[0];
+            if(command.equals(Commands.LIST_OF_FILES)) {
+                fileManager.listOfFiles(false);
+            }else if (command.equals(Commands.LIST_OF_FILES_WITH_SIZE)) {
+                fileManager.listOfFiles(true);
+            }else if (command.equals(Commands.COPY_FILE)) {
+                String sourceFileName = tokens[1];
+                String destFileName = tokens[2];
+                fileManager.copyFile(sourceFileName, destFileName);
+            }else if (command.equals(Commands.CREATE_FILE)) {
+                String fileName = tokens[1];
+                fileManager.createFile(fileName);
+            }else if (command.equals(Commands.FILE_CONTENT)) {
+                String fileName = tokens[1];
+                fileManager.content(fileName);
+            }else if (command.equals(Commands.CHANGE_DIRECTORY)) {
+                String newDirectory = tokens[1];
+                fileManager.changeDirectory(newDirectory);
+            }
         }
         
         scanner.close();
--- src/main/java/darvin/file_manager/Main.java
+++ src/main/java/darvin/file_manager/Main.java
@@ -3,9 +3,10 @@ package darvin.file_manager;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("I did it!!!!");
         InputOfUser input = new InputOfUser();
         input.input();
+        
+        
     }
 
 }
